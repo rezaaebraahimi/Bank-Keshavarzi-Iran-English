48dbe5771d65efcff8c615b9271b5fa6f689836c
reza_ebrahimi <rezaebrahimiproduction@gmail.com>
2024-03-10 00:44:03 +0330

    add script

---
+++ static/script/script.js
@@ -0,0 +1,17 @@
+window.addEventListener("load", function(){
+    setTimeout(
+        function open(event){
+            document.querySelector(".alert").style.display = "block";
+        },
+        100
+    )
+});
+
+window.addEventListener("load", function(){
+    setTimeout(
+        function close(event){
+            document.querySelector(".alert").style.display = "none";
+        },
+        3000
+    )
+});
